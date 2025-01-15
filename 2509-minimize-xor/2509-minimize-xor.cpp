6810efef5c3714c2cb7298efed26f4ae064fb567
Rishabh Kumar Mishra <161306539+rishabhmishra51@users.noreply.github.com>
2025-01-15 06:36:12 +0530

    Time: 0 ms (100%), Space: 8 MB (64.02%) - LeetHub

--- 2509-minimize-xor/2509-minimize-xor.cpp
+++ 2509-minimize-xor/2509-minimize-xor.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int minimizeXor(int num1, int num2) {
-        
-        int cnt = __builtin_popcount(num2);
-        int x = 0, ones = 0;
-        
-        for(int i = 31; i >= 0; --i) {
-            if(ones < cnt && (num1 & (1 << i)) != 0) {
-                
-                ++ones;
-                x |= (1 << i);
+        int count = __builtin_popcount(num2);
+
+        int res=0;
+        //Cancel set bits from highest to lowest weight
+        for(int i=31;i>=0 and count;i--){
+            if(num1 & (1<<i)){
+                count--;
+                res += (1<<i);
             }
         }
         
-        for(int i = 0; i <= 31; ++i) {
-            if(ones < cnt && (x & (1 << i)) == 0) {
-                
-                ++ones;
-                x |= (1 << i);
+        //Set bits lowest to heighest weight
+        for(int i=0;i<32 and count;i++){
+            if((num1 & (1<<i))==0){
+                count--;
+                res += (1<<i);
             }
         }
-        
-        return x;
+        return res;
     }
 };
