7d06181070f80aea0db8ff49a86647d75d3926e7
Rishabh Kumar Mishra <161306539+rishabhmishra51@users.noreply.github.com>
2025-03-03 21:27:28 +0530

    Time: 12 ms (25.48%), Space: 127.8 MB (71.24%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -2,30 +2,55 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n=nums.size();
-        int lesscount=0;
-        int equalcount=0;
+        // int lesscount=0;
+        // int equalcount=0;
 
-        for(int i=0;i<n;i++){
-            if(nums[i]==pivot) equalcount++;
-            else if(nums[i]<pivot) lesscount++;
-        }
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==pivot) equalcount++;
+        //     else if(nums[i]<pivot) lesscount++;
+        // }
+        // int i=0;
+        // int j=lesscount;
+        // int k=equalcount+lesscount;
+        // vector<int>ans(n);
+        // for(int &num :nums){
+        //     if(num < pivot){
+        //         ans[i]=num;
+        //         i++;
+        //     }
+        //     else if(num == pivot){
+        //         ans[j]=num;
+        //         j++;
+        //     }
+        //     else{
+        //         ans[k]=num;
+        //         k++;
+        //     }
+        // }
+        // return ans;
+        //O(n) tc and O(1) sc
+
+
         int i=0;
-        int j=lesscount;
-        int k=equalcount+lesscount;
+        int j=n-1;
+        int i_=0;
+        int j_=n-1;
         vector<int>ans(n);
-        for(int &num :nums){
-            if(num < pivot){
-                ans[i]=num;
-                i++;
+        while(i<n && j>=0){
+            if(nums[i]<pivot){
+                ans[i_]=nums[i];
+                i_++;
             }
-            else if(num == pivot){
-                ans[j]=num;
-                j++;
+            if(nums[j]>pivot){
+                ans[j_]=nums[j];
+                j_--;
             }
-            else{
-                ans[k]=num;
-                k++;
-            }
+            i++;
+            j--;
+        }
+        while(i_<=j_){
+            ans[i_]=pivot;
+            i_++;
         }
         return ans;
     }
