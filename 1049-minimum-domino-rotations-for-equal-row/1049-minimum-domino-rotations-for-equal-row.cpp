53857f8a7518f305a34cf85eeb3a244c33baec93
Rishabh Kumar Mishra <161306539+rishabhmishra51@users.noreply.github.com>
2025-05-04 00:01:15 +0530

    Time: 75 ms (5.17%), Space: 115.5 MB (48.71%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int res = getRotation(tops, bottoms, tops[0]);
-        if (tops[0] != bottoms[0]) {
-            res = min(res, getRotation(tops, bottoms, bottoms[0]));
+        int res = INT_MAX;
+
+        int face[7] = {};
+        for(int i = 0; i < tops.size(); i++) {
+            face[tops[i]]++;
+            if(bottoms[i] != tops[i]) face[bottoms[i]]++;
         }
-        return res == INT_MAX ? -1 : res;
-    }
 
-private:
-    int getRotation(vector<int>& tops, vector<int>& bottoms, int target) {
-        int rotateTop = 0;
-        int rotateBottom = 0;
+        // for each possible face from 1...6
+        for(int x = 1; x <= 6; x++) {
+            if(face[x] < tops.size()) continue;      // can't fill entire row
 
-        for (int i = 0; i < tops.size(); i++) {
-            if (tops[i] != target && bottoms[i] != target) {
-                return INT_MAX;
+            int maintainTop = 0, maintainBottom = 0;
+            bool possible = true;
+
+            // count flips
+            for(int i = 0; i < tops.size(); i++) {
+                if (tops[i] != x)      maintainTop++;
+                if (bottoms[i] != x)   maintainBottom++;
             }
-            if (tops[i] != target) rotateTop++;
-            if (bottoms[i] != target) rotateBottom++;
+
+            if (possible) 
+                res = min(res, min(maintainTop, maintainBottom));
         }
-        return min(rotateTop, rotateBottom);
+
+        return res == INT_MAX ? -1 : res;
     }
 };
