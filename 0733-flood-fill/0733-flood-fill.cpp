815a2d2a5f97e06e13d8a465a84fa9feea37836c
Rishabh Kumar Mishra <161306539+rishabhmishra51@users.noreply.github.com>
2025-08-31 14:16:46 +0530

    Time: 0 ms (100%), Space: 18.5 MB (16.7%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int sr, int sc,int color,int originalColor){
-  int m = image.size();
-        int n = image[0].size();
+    typedef pair<int,int> P;
 
-         if (sr < 0 || sr >= m || sc < 0 || sc >= n) return;
-        if (image[sr][sc] != originalColor) return;
-        image[sr][sc] = color;
-       dfs(image, sr + 1, sc, color, originalColor);
-       dfs(image, sr - 1, sc, color, originalColor);
-       dfs(image, sr , sc+1, color, originalColor);
-       dfs(image, sr , sc-1, color, originalColor);
-    }
-   typedef pair<int,int>P;
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m = image.size();
+        int n = image[0].size();
         int originalColor = image[sr][sc];
-        if (originalColor == color) return image;
-        dfs(image,sr,sc,color,originalColor);
+
+        if (originalColor == color) return image;  // avoid infinite loop
+
+          queue<P> q;
+        q.push({sr, sc});
+        image[sr][sc] = color;  // paint starting pixel
+
+        vector<vector<int>> directions{{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+        while (!q.empty()) {
+            P curr = q.front();  
+            q.pop();
+            int x = curr.first;
+            int y = curr.second;
+
+            for (auto& dir : directions) {
+                int new_x = x + dir[0];
+                int new_y = y + dir[1];
+
+                if (new_x >= 0 && new_x < m && new_y >= 0 && new_y < n 
+                    && image[new_x][new_y] == originalColor) {
+                    
+                    image[new_x][new_y] = color;
+                    q.push({new_x, new_y});
+                }
+            }
+        }
+
         return image;
     }
 };
