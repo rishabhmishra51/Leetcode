aa313303aaa29638dc4e7dabd45246787d77c336
Rishabh Kumar Mishra <161306539+rishabhmishra51@users.noreply.github.com>
2025-09-10 23:52:33 +0530

    Time: 46 ms (73.42%), Space: 33 MB (17.77%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,33 +1,40 @@
 class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>result;
-        for(int i=0;i<nums.size();i++){
-            if(i>0&& nums[i]==nums[i-1])
-                continue;
-            
-            int j=i+1;
-            int k = nums.size()-1;
-            while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum > 0) {
-                    k--;
-                }
-                else if(sum <0){
-                    j++;
-                }
-                else{
-                    result.push_back({nums[i],nums[j],nums[k]});
-                    j++;
-                
-                while(j<k && nums[j]==nums[j-1]){
-                    j++;
-                }
-              
-                }
+    private:
+    void two(vector<int>& nums,int target,int i,int j){
+        while(i<j){
+            if(nums[i]+nums[j]>target){
+                j--;
+            }
+            else if(nums[i]+nums[j] < target){
+                i++;
+            }
+            else{
+                while(i<j&&nums[i]==nums[i+1]) i++;
+                 while(i<j&&nums[j]==nums[j-1]) j--;
+                 ans.push_back({-target,nums[i],nums[j]});
+                 i++;
+                 j--;
+                 
             }
         }
-        return result;
+
+    }
+public:
+
+        vector<vector<int>>ans;
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        if(n<3) return {};
+        ans.clear();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(i>0&&nums[i]==nums[i-1]){
+                continue;
+            }
+            int n1=nums[i];
+            int target = -n1;
+            two(nums,target,i+1,n-1);
+        }
+        return ans;
     }
 };
