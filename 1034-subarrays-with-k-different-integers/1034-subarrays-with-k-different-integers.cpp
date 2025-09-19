435c4e61381c915dcc43cc895748bc555c69e26f
Rishabh Kumar Mishra <161306539+rishabhmishra51@users.noreply.github.com>
2025-09-19 10:31:20 +0530

    Time: 87 ms (46.22%), Space: 56.2 MB (60.91%) - LeetHub

---
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+  int slidingWindow(vector<int>& nums, int k){
+    unordered_map<int,int>mp;
+        int n=nums.size();
+        int i=0,j=0,count=0;
+        while(j<n){
+            mp[nums[j]]++;
+            while(mp.size()>k){
+                mp[nums[i]]--;
+                if(mp[nums[i]]==0){
+                  mp.erase(nums[i]);
+                }
+                  i++;
+            }
+            count += j-i+1;
+            j++;
+        }
+        return count;
+  }
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+       return slidingWindow(nums,k)-slidingWindow(nums,k-1);
+    }
+};
